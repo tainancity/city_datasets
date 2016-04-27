9b412f00c0dd4bc99db90e6450e719fef3ae4aad
kiang <kiange@gmail.com>
2016-04-27 17:19:54 +0800

    fix typo

--- Config/schema/schema.sql
+++ Config/schema/schema.sql
@@ -139,7 +139,7 @@ CREATE TABLE `links_tags` (
   `model` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `foreign_id` binary(16) DEFAULT NULL,
   `tag_id` binary(16) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 

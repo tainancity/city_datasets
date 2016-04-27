e01eba32412b7d225fa3e5bcceb22d0cd33d12b0
kiang <kiange@gmail.com>
2016-04-27 17:17:27 +0800

    latest schema

--- Config/schema/schema.sql
+++ Config/schema/schema.sql
@@ -1,123 +1,211 @@
-SET NAMES utf8;
+-- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: city_datasets
+-- ------------------------------------------------------
+-- Server version	5.6.28-0ubuntu0.15.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acos`
+--
 
 DROP TABLE IF EXISTS `acos`;
-CREATE TABLE IF NOT EXISTS `acos`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` int(11), 
-	`model` varchar(64), 
-	`foreign_key` int(11), 
-	`alias` varchar(128), 
-	`lft` int(11), 
-	`rght` int(11), 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `model` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_key` int(11) DEFAULT NULL,
+  `alias` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lft` int(11) DEFAULT NULL,
+  `rght` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `aros`
+--
 
 DROP TABLE IF EXISTS `aros`;
-CREATE TABLE IF NOT EXISTS `aros`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` int(11), 
-	`model` varchar(64), 
-	`foreign_key` int(11), 
-	`alias` varchar(128), 
-	`lft` int(11), 
-	`rght` int(11), 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `model` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_key` int(11) DEFAULT NULL,
+  `alias` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lft` int(11) DEFAULT NULL,
+  `rght` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `aros_acos`
+--
 
 DROP TABLE IF EXISTS `aros_acos`;
-CREATE TABLE IF NOT EXISTS `aros_acos`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`aro_id` int(11), 
-	`aco_id` int(11), 
-	`_create` int(2) DEFAULT NULL, 
-	`_read` int(2) DEFAULT NULL, 
-	`_update` int(2) DEFAULT NULL, 
-	`_delete` int(2) DEFAULT NULL, 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aros_acos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `aro_id` int(11) DEFAULT NULL,
+  `aco_id` int(11) DEFAULT NULL,
+  `_create` int(2) DEFAULT NULL,
+  `_read` int(2) DEFAULT NULL,
+  `_update` int(2) DEFAULT NULL,
+  `_delete` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `members`;
-CREATE TABLE IF NOT EXISTS `members`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`group_id` int(11), 
-	`username` varchar(64), 
-	`password` varchar(48), 
-	`user_status` varchar(1) DEFAULT 'N', 
-	`created` datetime, 
-	`modified` datetime, 
-	PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE IF NOT EXISTS `groups`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` int(11), 
-	`name` varchar(64), 
-	PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-DROP TABLE IF EXISTS `group_permissions`;
-CREATE TABLE IF NOT EXISTS `group_permissions`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` int(11), 
-	`order` int(11), 
-	`name` varchar(64), 
-	`description` varchar(255) DEFAULT NULL, 
-	`acos` varchar(255), 
-	PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-DROP TABLE IF EXISTS `datasets_tags`;
-CREATE TABLE IF NOT EXISTS `datasets_tags`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`Dataset_id` int(11), 
-	`Tag_id` int(11), 
-	PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Table structure for table `datasets`
+--
 
 DROP TABLE IF EXISTS `datasets`;
-CREATE TABLE IF NOT EXISTS `datasets`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`Organization_id` int(11), 
-	`parent_id` varchar(255) DEFAULT NULL, 
-	`name` varchar(255) DEFAULT NULL, 
-	`foreign_id` varchar(255) DEFAULT NULL, 
-	`foreign_uri` varchar(255) DEFAULT NULL, 
-	`lft` varchar(255) DEFAULT NULL, 
-	`rght` varchar(255) DEFAULT NULL, 
-	`created` varchar(255) DEFAULT NULL, 
-	`modified` varchar(255) DEFAULT NULL, 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `datasets` (
+  `id` binary(16) NOT NULL,
+  `organization_id` binary(16) NOT NULL,
+  `parent_id` binary(16) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_id` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_uri` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lft` bigint(16) DEFAULT NULL,
+  `rght` bigint(16) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `organizations_tags`;
-CREATE TABLE IF NOT EXISTS `organizations_tags`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`Organization_id` int(11), 
-	`Tag_id` int(11), 
-	PRIMARY KEY  (`id`)
+--
+-- Table structure for table `group_permissions`
+--
+
+DROP TABLE IF EXISTS `group_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `acos` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `links_tags`
+--
+
+DROP TABLE IF EXISTS `links_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `links_tags` (
+  `id` binary(16) NOT NULL,
+  `model` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_id` binary(16) DEFAULT NULL,
+  `tag_id` binary(16) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) DEFAULT NULL,
+  `username` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(48) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_status` varchar(1) COLLATE utf8_unicode_ci DEFAULT 'N',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organizations`
+--
 
 DROP TABLE IF EXISTS `organizations`;
-CREATE TABLE IF NOT EXISTS `organizations`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` varchar(255) DEFAULT NULL, 
-	`name` varchar(255) DEFAULT NULL, 
-	`foreign_id` varchar(255) DEFAULT NULL, 
-	`foreign_uri` varchar(255) DEFAULT NULL, 
-	`lft` varchar(255) DEFAULT NULL, 
-	`rght` varchar(255) DEFAULT NULL, 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `organizations` (
+  `id` binary(16) NOT NULL,
+  `parent_id` binary(16) DEFAULT NULL,
+  `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `foreign_id` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foreign_uri` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lft` bigint(16) DEFAULT NULL,
+  `rght` bigint(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags`
+--
 
 DROP TABLE IF EXISTS `tags`;
-CREATE TABLE IF NOT EXISTS `tags`(
-	`id` int(11) NOT NULL AUTO_INCREMENT, 
-	`parent_id` varchar(255) DEFAULT NULL, 
-	`name` varchar(255) DEFAULT NULL, 
-	`model` varchar(255) DEFAULT NULL, 
-	`lft` varchar(255) DEFAULT NULL, 
-	`rght` varchar(255) DEFAULT NULL, 
-	PRIMARY KEY  (`id`)
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` binary(16) NOT NULL,
+  `parent_id` binary(16) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `model` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lft` bigint(16) DEFAULT NULL,
+  `rght` bigint(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-27 17:16:27

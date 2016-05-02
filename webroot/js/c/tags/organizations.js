4a7a374f867319e908060b5c97052c51921dfe0f
kiang <kiange@gmail.com>
2016-05-02 08:21:16 +0800

    move js parts to independent file

---
+++ webroot/js/c/tags/organizations.js
@@ -0,0 +1,88 @@
+var selectedItem = false;
+$(function () {
+    $('input#datasetHelper').autocomplete({
+        source: function (request, response) {
+            currentTerm = request.term;
+            $.ajax({
+                url: queryUrl + request.term,
+                dataType: "json",
+                data: {},
+                success: function (data) {
+                    response(data.result);
+                }
+            });
+        },
+        select: function (event, ui) {
+            selectedItem = ui.item;
+        },
+        minLength: 1
+    });
+    $('input.tagItem').autocomplete({
+        source: function (request, response) {
+            currentTerm = request.term;
+            $.ajax({
+                url: queryUrl + request.term,
+                dataType: "json",
+                data: {},
+                success: function (data) {
+                    response(data.result);
+                }
+            });
+        },
+        select: function (event, ui) {
+            var tagId = $(this).attr('data-id');
+            $.get(tagSetUrl + ui.item.id + '/' + tagId + '/on');
+            $('#tagList' + tagId).append('<li class="ui-state-default" data-tag-id="' + tagId + '" id="' + ui.item.id + '">' + ui.item.name + '</li>');
+        },
+        minLength: 1
+    });
+    $('a#tagAdd').click(function () {
+        $.post(tagAddUrl, {Tag: {
+                model: 'Organization',
+                name: $('input#datasetHelper').val()
+            }}, function (r) {
+            if (r.result === 'ok') {
+                $.get(tagSetUrl + selectedItem.id + '/' + r.id + '/on', {}, function () {
+                    location.href = currentUrl;
+                });
+            }
+        }, 'json');
+        return false;
+    });
+    $("ul.droptrue").sortable({
+        connectWith: "ul"
+    });
+
+    $('input.tagName').change(function () {
+        var tagId = $(this).attr('data-id');
+        $.post(tagEditUrl + tagId, {Tag: {
+                name: $(this).val()
+            }}, function () {
+            showsave();
+        });
+    });
+
+    $("ul.droptrue").droppable({
+        tolerance: 'touch',
+        drop: function (event, ui) {
+            var oTagId = ui.draggable.attr('data-tag-id');
+            var nTagId = $(this).attr('data-tag-id');
+            var datasetId = ui.draggable.attr('id');
+            if (oTagId !== nTagId) {
+                ui.draggable.attr('data-tag-id', nTagId);
+                $(ui.draggable).detach().appendTo(this);//先把項目移過去
+                $.get(tagSetUrl + datasetId + '/' + oTagId + '/off');
+                $.get(tagSetUrl + datasetId + '/' + nTagId + '/on');
+                showsave();
+            }
+        }
+    });
+
+});
+function showsave()
+{
+    $('#savemsg').show();
+    setTimeout(function () {
+        $('#savemsg').fadeOut();
+    }, 2000);
+}

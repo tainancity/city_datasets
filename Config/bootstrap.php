fe769a501eb4debe67986aaba0b54f279ce1bbba
kiang <kiange@gmail.com>
2016-05-03 11:10:28 +0800

    remove city_to_str() as the root nodes were applied chinese names

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -16,27 +16,3 @@ function tag_to_str($tag_type_name)
         
     }
 }
-
-
-function city_to_str($city_name)
-{
-    switch ($city_name)
-    {
-        case "hccg":
-            return "新竹市";
-            break;
-        case "nantou":
-            return "南投縣";
-            break;
-        case "tainan":
-            return "臺南市";
-            break;
-        case "taipei":
-            return "臺北市";
-            break;
-        case "taoyuan":
-            return "桃園市";
-            break;
-        
-    }
-}

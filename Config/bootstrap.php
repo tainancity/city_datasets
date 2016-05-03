0fbb486bff132bff26c8835903dae6620b1abd39
shinryuken <shinryuken@mail.tainan.gov.tw>
2016-05-03 13:42:21 +0800

    加入縣市英文轉換成中文function

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,25 +3,19 @@
 CakePlugin::loadAll();
 require App::pluginPath('Permissible') . 'Config/init.php';
 
-function tag_to_str($tag_type_name)
-{
-    switch ($tag_type_name)
-    {
+function tag_to_str($tag_type_name) {
+    switch ($tag_type_name) {
         case "Organization":
             return "組織";
             break;
         case "Dataset":
             return "資料集";
             break;
-        
     }
 }
 
-
-function city_to_str($city_name)
-{
-    switch ($city_name)
-    {
+function city_to_str($city_name) {
+    switch ($city_name) {
         case "hccg":
             return "新竹市";
             break;
@@ -37,6 +31,5 @@ function city_to_str($city_name)
         case "taoyuan":
             return "桃園市";
             break;
-        
     }
 }
